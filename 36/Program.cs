3a6392b27ae0b03d3851db117d48e1a629ce1789
irina shkola <iri6kass@mail.ru>
2023-05-27 21:38:58 +0300

    ДЗ на проверку

---
+++ 36/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+int[] FillArrayWithRandomNumber(int size)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(10, 100);
+}
+return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+int sum = 0;
+
+for (int a = 1; a < array.Length; a+=2)
+sum=sum+array[a];
+Console.WriteLine($"Сумма чисел на нечетных позициях равна {sum}");
---
+++ 38/Program.cs
@@ -0,0 +1,42 @@
+﻿//  Задайте массив вещественных(тип double) чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3,1; 7,2; 22,3; 2,4; 78,5] -> 76,1
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] aray = new double[size];
+FillArrayRandomNumbers(aray);
+PrintArray(aray);
+double min = aray[0];
+double max = aray[0];
+
+for (int z = 0; z < aray.Length; z++)
+{
+    if (aray[z] > max)
+        {
+            max = aray[z];
+        }
+    if (aray[z] < min)
+        {
+            min = aray[z];
+        }
+}
+
+Console.WriteLine($"Max = {max}, Min = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] aray)
+{
+    for(int i = 0; i < aray.Length; i++)
+        {
+            aray[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] aray)
+{
+    
+    for(int i = 0; i < aray.Length; i++)
+        {
+            Console.Write(aray[i] + " ");
+        }
+    
+    Console.WriteLine();
+}
